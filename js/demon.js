d86fd3363a1b844ab9a7eef5b96ab30f9e2fdcdb
TwoPointNine <1185366392@qq.com>
2021-02-13 21:17:03 +0800

    Site updated: 2021-02-13 21:17:03

--- js/demon.js
+++ js/demon.js
@@ -1,2 +1,21 @@
 var Name=document.getElementById("name");
-var press=document.getElementById("press");
+var alterTip=document.getElementById("alterTip");
+var dayTime=document.getElementById("dayTime");
+var dayName=document.getElementById("dayName");
+var time=new Date();
+
+function click1(){
+    switch(Name.value){
+        case "":
+        case " ":{
+            console.log("输入为空");
+            alterTip.classList.add("showOutTips");
+        };break;
+        case "熊姝贤":{
+
+        };break;
+        default:{
+
+        };break;
+    }
+}
